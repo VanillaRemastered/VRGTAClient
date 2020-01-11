da14fb42a86bc1c36360f10082e3180da0fd6008
pointerboy <lukatrbovic@outlook.com>
2020-01-11 23:36:34 +0100

    Add project files.

---
+++ VanillaUpdater/Form1.cs
@@ -0,0 +1,33 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VanillaUpdater
+{
+    public partial class Form1 : MaterialForm
+    {
+        public MaterialSkinManager materialSkinManager { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.DARK;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+    }
+}
