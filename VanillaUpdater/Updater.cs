203fe572a5d6e2e5b212cfcb5aff32d3a0869fbd
pointerboy <lukatrbovic@outlook.com>
2020-01-12 13:07:10 +0100

    implemented update class from vrshaleper

---
+++ VanillaUpdater/Updater.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Runtime.Serialization;
+
+namespace VanillaUpdater
+{
+    class Updater
+    {
+        public static string GetFileViaHttpString(string url)
+        {
+            using (WebClient client = new WebClient())
+            {
+                return client.DownloadString(url);
+            }
+        }
+
+        private static void FetchVersion()
+        {
+            var versionObjRaw = GetFileViaHttpString("http://www.vanilla-remastered.com/files/latest.json");
+            UpdateData update = JsonConvert.DeserializeObject<UpdateData>(versionObjRaw);
+        }
+
+        public static bool IsNewVersionAvailable()
+        {
+            FetchVersion();
+            if (VRegistry.GetSubKeyValue("Version").ToString() != UpdateData.Version)
+                return true;
+            return false;
+        }
+    }
+
+
+    [DataContract]
+    class UpdateData
+    {
+        [DataMember]
+        public static string Version { get; set; }
+
+        [DataMember]
+        public static string Author { get; set; }
+
+        [DataMember]
+        public static string[] Changes { get; set; }
+
+        [DataMember]
+        public static DateTime Date { get; set; }
+
+        [DataMember]
+        public static string[] AffectedFiles { get; set; }
+
+        [DataMember]
+        public static string DownloadURL { get; set; }
+    }
+}
