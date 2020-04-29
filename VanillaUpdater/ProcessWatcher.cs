1c7f64f45506bb5444c63f4eb10ec4bb968e386f
pointerboy <lukatrbovic@outlook.com>
2020-04-29 18:53:59 +0200

    conflict from v2 fixed

--- VanillaUpdater/ProcessWatcher.cs
+++ VanillaUpdater/ProcessWatcher.cs
@@ -4,12 +4,24 @@ namespace VanillaUpdater
 {
     internal class ProcessWatcher
     {
+        public enum GameType
+        {
+            SA = 0,
+            VC = 1
+        }
+
+        public static bool IsGameRunning(GameType game)
+        {
+            var pname = Process.GetProcessesByName("gta_" + game.ToString().ToLower());
+            if (pname.Length == 0) return false;
+            return true;
+        }
         /// <summary>
         /// Checks if the game is running or not
         /// </summary>
-        public static bool IsGameRunning()
+        public static bool IsSARunning()
         {
-           var pname = Process.GetProcessesByName("gta_sa");
+            var pname = Process.GetProcessesByName("gta_sa");
             if (pname.Length == 0) return false;
             return true;
         }
